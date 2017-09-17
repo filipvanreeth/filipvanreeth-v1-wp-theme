264704f14690c6760ea3b0b9d60540b192389b15
filipvanreeth <filip@webbird.be>
2017-09-17 11:39:14 +0200

    Journal archive

--- index.php
+++ index.php
@@ -38,53 +38,8 @@
 
 <?php get_template_part( 'template-parts/expertises' ) ?>
 
-<div class="journal--archive">
-  <div class="journal--archive__content">
-    <p class="journal--archive__content__icon">
-      <img src="https://png.icons8.com/typewriter-with-tablet/ios7/75" title="Typewriter With Tablet">
-    </p>
-    <h1>I Journal.</h1>
-    <p class="text--medium introduction">Every now and then I sit down and write something that will challenge or encourage my and hopefully your WordPress experience.</p>
-    <ul class="journal--archive__content__list">
-      <li class="journal--archive__content__list__item">
-        <div class="journal--archive__content__list__item__date">
-          <?php echo the_time('F d, Y') ?>
-        </div>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </li>
-      <li class="journal--archive__content__list__item journal--archive__content__list__item--spotlight">
-        <div class="journal--archive__content__list__item__date">
-          <?php echo the_time('F d, Y') ?>
-        </div>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </li>
-      <li class="journal--archive__content__list__item">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </li>
-      <li class="journal--archive__content__list__item">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </li>
-      <li class="journal--archive__content__list__item">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </li>
-      <li class="journal--archive__content__list__item journal--archive__content__list__item--more-newsletter">
-        <img src="https://png.icons8.com/keyboard/ios7/50" title="Keyboard" width="50" height="50">
-        <h3>I hope you enjoy the <a href="#">456 articles</a> that I've written so far.</h3>
-        <p>If so you can follow me through social media, subscribe to my newsletter or just reach out for a passion-to-heart talk.</p>
-      </li>
-    </ul>
-  </div>
-  <div class="journal--archive__video">
-    <video src="<?php echo get_stylesheet_directory_uri() ?>/videos/485001612-typewriter.mp4" autoplay loop poster="posterimage.jpg">
+<?php get_template_part( 'template-parts/journal-archive' ) ?>
 
-    </video>
-  </div>
-</div>
 
 <div class="menus">
   <ul class="menus__list">
